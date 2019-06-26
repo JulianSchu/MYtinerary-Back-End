79b8196d63c125d1577da9f7bdec44195e8afbdf
JulianSchu <jun.xu@hotmail.dem>
2019-06-26 10:58:05 +0200

    login in page front end done

---
+++ routes/models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    userName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    profilPic: {
+        type: String
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    passwordConfirm: {
+        type: String,
+    }
+});
+
+module.exports = mongoose.model('user', userSchema)
